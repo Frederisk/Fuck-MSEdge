1a8dc15164f861ea2e86ec100f7cc1c8f73823ae
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2024-02-02 18:00:30 +0800

    Update Fuck-MSEdge.ps1

--- Fuck-MSEdge.ps1
+++ Fuck-MSEdge.ps1
@@ -3,6 +3,7 @@
 #Requires -RunAsAdministrator
 
 using namespace System;
+using namespace System.IO;
 using namespace System.Security.Principal;
 
 
@@ -13,18 +14,22 @@ using namespace System.Security.Principal;
 #     # $ParameterName
 # )
 
-# Is Windows Powershell
+[String]$SetupFile = '';
+[String]$WebviewSetupFile = '';
+[Switch]$AllowForceRemove = '';
 
-[Boolean]$isWindowsPowershell = ($PSVersionTable.PSVersion.Major -eq 5); # is version 5.*
+
+
+# Is Windows Powershell
+[Boolean]$isWindowsPowershell = ($PSVersionTable.PSVersion.Major -le 5); # is version 5.*
 
 # Check am I Admin?
 [WindowsPrincipal]$principal = [WindowsPrincipal]::new([WindowsIdentity]::GetCurrent());
 [Boolean]$isAdmin = $principal.IsInRole([WindowsBuiltInRole]::Administrator);
-
-# if (-not $isAdmin){
-#     Write-Error -Message 'You must execute this script as an administrator.';
-#     exit 1;
-# }
+if (-not $isAdmin) {
+    Write-Error -Message 'You must execute this script as an administrator.';
+    exit 1;
+}
 
 ## part 1 remove Appx version
 
@@ -45,13 +50,55 @@ else {
 
 # Deprovisioned Edge
 $edgeAppxPackageNameList | ForEach-Object -Process {
-    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$uid\$_" -Force;
-    New-Item -Path "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\S-1-5-18\$_" -Force;
-    New-Item -Path "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\$_" -Force;
-};
-
-# Remove Edge
-$edgeAppxPackageNameList | ForEach-Object -Process {
+    New-Item -Path "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$uid\$_" -Force | Out-Null;
+    New-Item -Path "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\S-1-5-18\$_" -Force | Out-Null;
+    New-Item -Path "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\$_" -Force | Out-Null;
+    # Remove package
     Remove-AppxPackage -Package $_;
     Remove-AppxPackage -AllUsers -Package $_;
 };
+
+## Part 2 Edge Main
+
+[String]$edgeRootDir = [Path]::Combine(${env:ProgramFiles(x86)}, 'Microsoft');
+[String]$edgeName = 'Edge';
+[String]$edgeVersion = Get-ChildItem -Path ([Path]::Combine($edgeRootDir, $edgeName, 'Application')) -Name | Where-Object -FilterScript { $_ -match '^\d+\.\d+\.\d+\.\d+$' };
+
+[String]$coreName = 'EdgeCore';
+[String]$coreVersion = Get-ChildItem -Path ([Path]::Combine($edgeRootDir, $coreName)) -Name | Where-Object -FilterScript { $_ -match '^\d+\.\d+\.\d+\.\d+$' };
+
+[String]$updateName = 'EdgeUpdate';
+[String]$updateVersion = Get-ChildItem -Path ([Path]::Combine($edgeRootDir, $updateName)) -Name | Where-Object -FilterScript { $_ -match '^\d+\.\d+\.\d+\.\d+$' };
+
+[String]$webviewName = 'EdgeWebView';
+[String]$webviewVersion = Get-ChildItem -Path ([Path]::Combine($edgeRootDir, $webviewName, 'Application')) -Name | Where-Object -FilterScript { $_ -match '^\d+\.\d+\.\d+\.\d+$' };
+
+if (-not [String]::IsNullOrWhiteSpace($SetupFile)) {
+    if (Test-Path -Path $SetupFile) {
+        $SetupFile = [Path]::GetFullPath($SetupFile);
+    }
+    else {
+        Write-Error -Message "Setup file not found: $SetupFile";
+        exit 1;
+    }
+}
+else {
+    # C:\Program Files (x86)\Microsoft\ Edge\ Application\92.0.902.67\Installer\setup.exe
+    $SetupFile = [Path]::Combine($edgeRootDir, $edgeName, 'Application', $edgeVersion, 'Installer', 'setup.exe');
+}
+
+if (Test-Path -Path $SetupFile) {
+    $setup_exe = Get-Command -Name $SetupFile;
+    # setup.exe --uninstall --system-level --force-uninstall
+    &$setup_exe --uninstall --system-level --force-uninstall;
+    Start-Sleep -Seconds 3;
+    if (Test-Path -Path $SetupFile) {
+        if ($AllowForceRemove) {
+            Remove-Item -Path ([Path]::Combine($edgeRootDir, $edgeName)) -Recurse -Force;
+        }
+        else {
+            Write-Error -Message 'Cannot remove Edge by `setup.exe`';
+            Write-Error -Message 'Some newer versions of Edge no longer allow users to uninstall it by `setup.exe` that is located in the installation directory. You can use the `--AllowForceRemove` switch to force remove the installation directory, or use the `--SetupFile` parameter to specify the setup file that still has this functionality.';
+        }
+    }
+}
